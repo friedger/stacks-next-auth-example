88ec8dafe052e8709c22186e9dab1c1cbeeedfca
Friedger Müffke <mail@friedger.de>
2022-07-25 17:34:39 +0200

    clarify todos

--- src/pages/api/examples/jwt.ts
+++ src/pages/api/examples/jwt.ts
@@ -2,17 +2,27 @@
 import { getToken } from "next-auth/jwt";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { createJWT, decodeJWT, ES256KSigner, hexToBytes } from "did-jwt";
-import { EthrDID } from 'ethr-did'
-import { createVerifiableCredentialJwt, Issuer, JwtCredentialPayload } from 'did-jwt-vc'
+import { EthrDID } from "ethr-did";
+import {
+  createVerifiableCredentialJwt,
+  Issuer,
+  JwtCredentialPayload,
+} from "did-jwt-vc";
 
 const secret = process.env.NEXTAUTH_SECRET;
 if (!secret) throw new Error("Secret missing");
 
-
+// TODO use StacksDID
 const issuer = new EthrDID({
-  identifier: '0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198',
-  privateKey: 'd8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75'
-}) as Issuer
+  identifier: "0xf1232f840f3ad7d23fcdaa84d6c66dac24efb198",
+  privateKey:
+    "d8b595680851765f38ea5405129244ba3cbad84467d190859f4c8b20c1ff6c75",
+}) as Issuer;
+
+const userToDID = (_: string) => {
+  // TODO decide on did method
+  return "did:ethr:0x435df3eda57154cf8cf7926079881f2912f54db4";
+};
 
 const createVerifiableCredential = async (
   req: NextApiRequest,
@@ -24,32 +34,28 @@ const createVerifiableCredential = async (
     const tweet = await fetchTweet(stxAddress);
     const twitterHandle = await verifiedTwitterHandle(tweet, stxAddress);
 
+    const sub = userToDID(stxAddress);
     const vcPayload: JwtCredentialPayload = {
-      sub: 'did:ethr:0x435df3eda57154cf8cf7926079881f2912f54db4',
-      nbf: 1562950282,
+      sub,
+      nbf: new Date().getTime(),
       vc: {
-        '@context': ['https://www.w3.org/2018/credentials/v1'],
-        type: ['VerifiableCredential'],
+        "@context": ["https://www.w3.org/2018/credentials/v1"],
+        type: ["VerifiableCredential"],
+        // TODO define credential schema
         credentialSubject: {
-          degree: {
-            type: 'BachelorDegree',
-            name: 'Baccalauréat en musiques numériques'
-          }
-        }
-      }
-    }
-
-    const vcJwt = await createVerifiableCredentialJwt(vcPayload, issuer)
-
-    const signedVerifiedCredential = await createJWT(
-      {
-        aud: "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74",
-        exp: 1957463421,
-        name: "uPort Developer",
+          accountHandle: {
+            service: "https://twitter.com",
+            user: twitterHandle,
+          },
+        },
       },
-      { issuer: "did:ethr:0xf3beac30c498d9e26865f34fcaa57dbb935b0d74", signer },
-      { alg: "ES256K" }
+    };
+
+    const signedVerifiedCredential = await createVerifiableCredentialJwt(
+      vcPayload,
+      issuer
     );
+
     res.send(JSON.stringify(signedVerifiedCredential, null, 2));
   }
 };
@@ -61,7 +67,5 @@ async function fetchTweet(stxAddress: string) {
 async function verifiedTwitterHandle(tweet: string, stxAddress: string) {
   return "@twitterHandle";
 }
-
-async function issueCredential() {}
 
 export default createVerifiableCredential;
